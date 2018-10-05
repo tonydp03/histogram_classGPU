bede1f8d465fdc6fe187fbf77ea3bcc151d93367
Tony Di Pilato <tony.dipilato03@gmail.com>
2018-10-05 16:47:04 +0200

    Histogram class

--- GPUHist2D.h
+++ GPUHist2D.h
@@ -1,72 +1,66 @@
-#include <iostream>
-#include <vector>
 #include <memory>
 #include <cmath>
 #include "GPUVecArray.h"
 
+#ifndef GPUHist2D_h
+#define GPUHist2D_h
+
 template <class T, int xDim, int yDim, int max_depth> struct histogram2D{
 
-  __host__ __device__
-  void init(float xMin, float xMax, float yMin, float yMax)
+  histogram2D(float xMin, float xMax, float yMin, float yMax)
   {
-    limits_[0]=xMin;
-    limits_[1]=xMax;
-    limits_[2]=yMin;
-    limits_[3]=yMax;
-    for(int i =0; i < xDim*yDim; i++)
+    limits_[0] = xMin;
+    limits_[1] = xMax;
+    limits_[2] = yMin;
+    limits_[3] = yMax;
+    data_.resize(xDim*yDim);
+    for(int i = 0; i < xDim*yDim; i++)
       data_[i].reset();
+    xBinSize_ = (xMax-xMin)/xDim;
+    yBinSize_ = (yMax-yMin)/yDim;
   }
 
-  __host__  __device__
-  bool fillBin(float x, float y, const T idx)
+  __host__
+  bool fillBin(float x, float y, T idx)
   {
     int xBin = computeXBinIndex(x);
     int yBin = computeYBinIndex(y);
-    data_[xBin + yBin*xDim].push_back(idx);
-    if(data_[xBin + yBin*xDim].push_back(idx) != -1)
+    if(data_[xBin + yBin*xDim].push_back_unsafe(idx) != -1)
       return true;
     else
       return false;
   }
 
+  __device__
+  bool fillBinGPU(float x, float y, T idx)
+  {
+    int xBin = computeXBinIndex(x);
+    int yBin = computeYBinIndex(y);
+    if(data_[xBin + yBin*xDim].push_back(idx) != -1)
+      return true;
+    else
+      return false;
+  }
+  
   __host__ __device__
   int computeXBinIndex(float x)
   {
-    int xIndex = floor((std::abs(x) - limits_[0]) / binSize_);
+    int xIndex = floor((std::abs(x) - limits_[0]) / xBinSize_);
     return xIndex;
   }
 
   __host__ __device__
   int computeYBinIndex(float y) 
   {
-    int yIndex = floor((y + limits_[3]) / binSize_);
+    int yIndex = floor((y + limits_[3]) / yBinSize_);
     return yIndex;
   } 
 
   __host__ __device__
-  void printBinContent(float x, float y)
+  int getBinIdx(float x, float y)
   {
     int xBin = computeXBinIndex(x);
     int yBin = computeYBinIndex(y);
-    
-    for(int i = 0; i < max_depth; i++)
-      printf("* Element: %f %f %d %d\n", x, y, xBin + yBin*xDim, data_[xBin + yBin*xDim][i]);
-  }
-
-  __host__ __device__
-  void printBinContent2(int x, int y)
-  {
-    for(int i = 0; i < max_depth; i++)
-      printf("*Element", data_[x + y*xDim][i]);
-   }
-
-  __host__ __device__
-  int getBinId(float x, float y)
-  {
-    int xBin = computeXBinIndex(x);
-    int yBin = computeYBinIndex(y);
-    //std::cout << "xBin: " << xBin << " yBin: " << yBin << std::endl;
-    //std::cout << "Abs(eta): " << std::abs(x) << std::endl;
     return xBin + yBin*xDim;
   }
 
@@ -79,7 +73,7 @@ template <class T, int xDim, int yDim, int max_depth> struct histogram2D{
   }
 
   __host__ __device__
-  std::array<int, 4> searchBox(float xMin, float xMax, float yMin, float yMax)
+  std::array<int,4> searchBox(float xMin, float xMax, float yMin, float yMax)
   {
     int xBinMin = xMin <= limits_[0] ? computeXBinIndex(limits_[0]) : computeXBinIndex(xMin);
     int xBinMax = xMax >= limits_[1] ? computeXBinIndex(limits_[1]) : computeXBinIndex(xMax);
@@ -92,11 +86,15 @@ template <class T, int xDim, int yDim, int max_depth> struct histogram2D{
     box[3] = yBinMax - yBinMin+ 1;
     return box;
   }
-  
+
   inline constexpr int size() const { return data_.size(); }
   inline constexpr GPU::VecArray<T, max_depth>& operator[](int i) { return data_[i]; }
-  
+
   GPU::VecArray<GPU::VecArray<T, max_depth>, xDim*yDim> data_;
-  std::array<float, 4> limits_;
-  float binSize_ = 0.05;  
+  float limits_[4];
+  float xBinSize_ = 0.0;
+  float yBinSize_ = 0.0;
+  
 };
+  
+#endif
